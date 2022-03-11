08d245bd289758064940985887392ffb4157b312
Batuhan Karaduman <b.karaduman@azerion.com>
2022-03-11 17:46:00 +0300

    magic numbers refactoring

---
+++ CodeRefactoring/MagicNumbers/Refactoring/DocumentStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeRefactoring.MagicNumbers.Refactoring
+{
+    public enum DocumentStatus
+    {
+        Draft = 1,
+        Lodged = 2
+    }
+}
